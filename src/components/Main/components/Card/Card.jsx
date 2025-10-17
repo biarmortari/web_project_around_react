a0520e9bc4f5c28684b7b3c23e8660336ec738c4
biarmortari <beatrizrmortari@gmail.com>
2025-10-17 08:06:50 -0300

    fix(card): refactor props destructuring and fix button closing tags

--- src/components/Main/components/Card/Card.jsx
+++ src/components/Main/components/Card/Card.jsx
@@ -1,35 +1,40 @@
-import ImagePopup from "../Popup/ImagePopup/ImagePopup";
-function Card(props) {
-  const { name, link } = props.card;
-  const { onImageClick } = props;
-  const imageComponent = {
-    title: null,
-    children: <ImagePopup card={props.card} />,
-  };
+import React from "react";
+import CurrentUserContext from "../../../../contexts/CurrentUserContext";
+
+function Card({ card, onImageClick, onDeleteCard, onCardLike }) {
+  const currentUser = React.useContext(CurrentUserContext);
+  const { name, link, likes = [] } = card;
+
+  const isLiked = likes.some((userId) => userId === currentUser._id);
+
+  const cardLikeButtonClassName = `card__like-button ${
+    isLiked ? "card__like-button_is-active" : ""
+  }`;
+
   return (
-    <>
-      <li className="card">
-        <img
-          className="card__image"
-          src={link}
-          alt={`Foto de ${name}`}
-          onClick={() => onImageClick(imageComponent)}
-        />
+    <li className="card">
+      <img
+        className="card__image"
+        src={link}
+        alt={`Foto de ${name}`}
+        onClick={() => onImageClick(card)}
+      />
+      <button
+        aria-label="Delete card"
+        className="card__delete-button"
+        type="button"
+        onClick={() => onDeleteCard(card)}
+      ></button>
+      <div className="card__description">
+        <h2 className="card__title">{name}</h2>
         <button
-          aria-label="Delete card"
-          className="card__delete-button"
+          aria-label="Like card"
           type="button"
-        />
-        <div className="card__description">
-          <h2 className="card__title">{name}</h2>
-          <button
-            aria-label="Like card"
-            type="button"
-            className="card__like-button"
-          />
-        </div>
-      </li>
-    </>
+          className={cardLikeButtonClassName}
+          onClick={() => onCardLike(card)}
+        ></button>
+      </div>
+    </li>
   );
 }
 
